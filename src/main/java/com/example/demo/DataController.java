216560dbe950fa7aa0f0c834f9c7798e9980eae7
Ammon King <ammon.king@woz-u.com>
2019-06-17 10:58:03 -0700

    added sayhello method

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.time.LocalTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class DataController {
 	@GetMapping()
 	public String getTime() {
 		return "The current time is: " + LocalTime.now().toString();
+	}
+	
+	@GetMapping("{:name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hello " + name;
 	}
 	
 }
