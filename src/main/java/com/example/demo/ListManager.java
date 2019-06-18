311ca07b06b651f8ec6092a0533c5e0615406c40
Ammon King <ammon.king@woz-u.com>
2019-06-18 10:03:06 -0700

    introduced a bug

---
+++ src/main/java/com/example/demo/ListManager.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListManager {
+	
+	private List<String> items;
+	
+	public ListManager() {
+		this.items = new ArrayList<>();
+	}
+	
+	public void addItem(String item) {
+		this.items.add(item);
+	}
+	
+	public void removeItem(String item) {
+		// do nothing
+	}
+	
+	public List<String> getItems(){
+		return this.items;
+	}
+	
+	
+}
